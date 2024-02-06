166c187458148b7047dadc86dffb929c5ba7882c
lifan <lifany@andrew.cmu.edu>
2024-02-06 13:15:44 -0500

    +

--- assignment1-master/solutions_lifany.py
+++ assignment1-master/solutions_lifany.py
@@ -7,10 +7,12 @@ from starter.utils import get_device, get_mesh_renderer, load_cow_mesh
 import imageio
 import numpy as np
 from PIL import Image as im 
+from PIL import ImageDraw
+from tqdm.auto import tqdm
 
 save_path = "results/"
 
-### Q 1.1 ###
+### Q 1.1 360-degree Renders ###
 def render_cow_360d(
     cow_path="data/cow.obj", 
     image_size=256, 
@@ -47,8 +49,8 @@ def render_cow_360d(
 
     res = []
 
-    # Prepare the camera:
     for angle in range(-180, 180, angle_step):
+        # Prepare the camera:
         R, T = pytorch3d.renderer.look_at_view_transform(dist=3, elev=0, azim=angle)
         cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
 
@@ -67,9 +69,53 @@ def render_cow_360d(
 
     imageio.mimsave(save_path + fname, res, fps=fps)
 
+### Q 1.2 Re-creating the Dolly Zoom ###
+def dolly_zoom(
+    image_size=256,
+    num_frames=10,
+    duration=3,
+    device=None,
+    output_file="output/dolly.gif",
+):
+    if device is None:
+        device = get_device()
+
+    mesh = pytorch3d.io.load_objs_as_meshes(["data/cow_on_plane.obj"])
+    mesh = mesh.to(device)
+    renderer = get_mesh_renderer(image_size=image_size, device=device)
+    lights = pytorch3d.renderer.PointLights(location=[[0.0, 0.0, -3.0]], device=device)
+
+    fovs = torch.linspace(5, 120, num_frames)
+
+    renders = []
+    for fov in tqdm(fovs):
+        distance = 5/(2*np.tan(0.5*np.radians(fov)) ) # TODO: change this.
+        T = [[0, 0, distance]]  # TODO: Change this.
+        cameras = pytorch3d.renderer.FoVPerspectiveCameras(fov=fov, T=T, device=device)
+        rend = renderer(mesh, cameras=cameras, lights=lights)
+        rend = rend[0, ..., :3].cpu().numpy()  # (N, H, W, 3)
+        renders.append(rend)
+
+    images = []
+    for i, r in enumerate(renders):
+        image = im.fromarray((r * 255).astype(np.uint8))
+        draw = ImageDraw.Draw(image)
+        draw.text((20, 20), f"fov: {fovs[i]:.2f}", fill=(255, 0, 0))
+        images.append(np.array(image))
+    imageio.mimsave(output_file, images, fps=(num_frames / duration))
+
 
 if __name__ == "__main__":
-    # Q1.1
+    # Q 1.1
     render_cow_360d(image_size = 1024)
 
-    
+    # Q 1.2
+    dolly_zoom(
+        image_size=1024,
+        num_frames=30,
+        duration=3,
+        output_file=save_path + "q1_2.gif"
+    )
+
+    # Q 2.1
+
