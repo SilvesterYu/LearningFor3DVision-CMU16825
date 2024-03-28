7b33badeafd50fbe46cdcae9e000b9d9b8a05034
lifan <lifany@andrew.cmu.edu>
2024-03-28 12:46:03 -0400

    render img size 64

---
+++ proj4/loop_forever.py
@@ -0,0 +1,16 @@
+
+from PIL import Image
+import os
+
+# make gifs loop forever
+target_dir = "Q1/"
+gifs = os.listdir(target_dir)
+print(gifs)
+
+for item in gifs:
+    print(item)
+    if ".gif" in item:
+        g = Image.open(target_dir + item)
+        g.save("results/" + item, save_all=True, loop=0)
+
+print("done!")
